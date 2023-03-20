f25313d21cc55e7a316124d18efbc5f048a4743c
Noman Ali <alinoman.itz@gmail.com>
2023-03-21 01:19:08 +0530

    Rem

--- reserver/init_schema.sql
+++ reserver/init_schema.sql
@@ -10,12 +10,3 @@ CREATE TABLE users (
     gender CHAR(1) NOT NULL,
     age INTEGER NOT NULL
 );
-
-DROP TABLE IF EXISTS admins;
-
-CREATE TABLE admins (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username VARCHAR UNIQUE NOT NULL,
-    password VARCHAR NOT NULL,
-    email VARCHAR NOT NULL
-);
