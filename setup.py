722733284595b4ddbc94a6def72530b1476d68d7
Shashwat Garg <gargshashwat007@gmail.com>
2023-04-09 18:21:44 +0530

    Added summary

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="Reserver",
-    version="0.0.5",
+    version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         "flask",
         "requests",
+        "matplotlib",
     ],
 )
